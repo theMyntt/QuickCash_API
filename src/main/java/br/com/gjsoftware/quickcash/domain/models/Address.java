a316bf2f8d873e96c4fd19e70f42f472e8bf7c08
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-21 14:17:59 -0300

    chore: not null and not empty annotations

--- src/main/java/br/com/gjsoftware/quickcash/domain/models/Address.java
+++ src/main/java/br/com/gjsoftware/quickcash/domain/models/Address.java
@@ -1,5 +1,7 @@
 package br.com.gjsoftware.quickcash.domain.models;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +12,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Address {
+    @NotNull(message = "Street should be not null")
+    @NotEmpty(message = "Street should be not empty")
     private String street;
+
+    @NotNull(message = "Zip Code should be not null")
+    @NotEmpty(message = "Zip Code should be not empty")
     private String zipCode;
+
+    @NotNull(message = "Country should be not null")
+    @NotEmpty(message = "Country should be not empty")
     private String country;
+
+    @NotNull(message = "State should be not null")
+    @NotEmpty(message = "State should be not empty")
     private String state;
 }
--- src/main/java/br/com/gjsoftware/quickcash/domain/models/Documents.java
+++ src/main/java/br/com/gjsoftware/quickcash/domain/models/Documents.java
@@ -1,5 +1,7 @@
 package br.com.gjsoftware.quickcash.domain.models;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,11 +14,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Documents {
     @Size(max = 11)
+    @NotNull(message = "CPF should be not null")
+    @NotEmpty(message = "CPF should be not empty")
     private String cpf;
 
     @Size(max = 9)
+    @NotNull(message = "RG should be not null")
+    @NotEmpty(message = "RG should be not empty")
     private String rg;
 
     @Size(max = 5)
+    @NotNull(message = "State of RG should be not null")
+    @NotEmpty(message = "State of RG should be not empty")
     private String state;
 }
