8eb745b1db604780dceddcb8bdc71792f4aa8f03
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-21 14:54:01 -0300

    feat: findBy email query

--- src/main/java/br/com/gjsoftware/quickcash/infrastructure/repositories/IClientRepository.java
+++ src/main/java/br/com/gjsoftware/quickcash/infrastructure/repositories/IClientRepository.java
@@ -2,6 +2,9 @@ package br.com.gjsoftware.quickcash.infrastructure.repositories;
 
 import br.com.gjsoftware.quickcash.infrastructure.schemas.Client;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface IClientRepository extends MongoRepository<Client, String> {
+    @Query("{ 'email': ?0 }")
+    Client findBy (String email);
 }
