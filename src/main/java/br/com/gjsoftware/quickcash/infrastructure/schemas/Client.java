1b5896aeeede79f001c50e5d82bc3cf4b9aa3c13
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-21 13:42:17 -0300

    chore: implementing document and id annotations

--- src/main/java/br/com/gjsoftware/quickcash/infrastructure/schemas/Client.java
+++ src/main/java/br/com/gjsoftware/quickcash/infrastructure/schemas/Client.java
@@ -6,12 +6,16 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Document
 public class Client {
+    @Id
     private String _id;
     private String name;
     private String email;
