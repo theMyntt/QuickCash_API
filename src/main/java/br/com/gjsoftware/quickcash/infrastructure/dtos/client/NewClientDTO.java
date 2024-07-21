8889619e9dbb118d631254d5efdeeabffe4490f6
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-21 15:44:33 -0300

    docs: schema examples for NewClientDTO.java

--- src/main/java/br/com/gjsoftware/quickcash/infrastructure/dtos/client/NewClientDTO.java
+++ src/main/java/br/com/gjsoftware/quickcash/infrastructure/dtos/client/NewClientDTO.java
@@ -3,6 +3,7 @@ package br.com.gjsoftware.quickcash.infrastructure.dtos.client;
 import br.com.gjsoftware.quickcash.domain.enums.Gender;
 import br.com.gjsoftware.quickcash.domain.models.Address;
 import br.com.gjsoftware.quickcash.domain.models.Documents;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -22,16 +23,19 @@ public class NewClientDTO {
     @NotNull(message = "Name should be not null")
     @NotEmpty(message = "Name should be not empty")
     @Size(min = 2, message = "Name should have more than 2 chars")
+    @Schema(example = "John Doe")
     private String name;
 
     @NotNull(message = "Email should be not null")
     @NotEmpty(message = "Email should be not empty")
     @Pattern(regexp = "^[A-Za-z0-9+_.-]+@(.+)$", message = "Email should be valid")
+    @Schema(example = "johndoe@example.com")
     private String email;
 
     @NotNull(message = "Password should be not null")
     @NotEmpty(message = "Password should be not empty")
     @Size(min = 2, message = "Password should have more than 8 chars")
+    @Schema(example = "12345678")
     private String password;
 
     @NotNull
