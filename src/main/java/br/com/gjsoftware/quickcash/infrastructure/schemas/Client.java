2048df11dae89d3c909f20f305fcae971d436391
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-21 13:40:14 -0300

    feat: clients entity

---
+++ src/main/java/br/com/gjsoftware/quickcash/infrastructure/schemas/Client.java
@@ -0,0 +1,22 @@
+package br.com.gjsoftware.quickcash.infrastructure.schemas;
+
+import br.com.gjsoftware.quickcash.domain.models.Address;
+import br.com.gjsoftware.quickcash.domain.models.Documents;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client {
+    private String _id;
+    private String name;
+    private String email;
+    private String password;
+    private double Money;
+    private Documents documents;
+    private Address address;
+}
