9a9d924241b0945811974d4b2cbb9f2736e03e9d
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-21 13:03:51 -0300

    feat: initial commit

---
+++ src/main/java/br/com/gjsoftware/quickcash/QuickcashApplication.java
@@ -0,0 +1,13 @@
+package br.com.gjsoftware.quickcash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickcashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickcashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gjsoftware/quickcash/QuickcashApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gjsoftware.quickcash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickcashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
