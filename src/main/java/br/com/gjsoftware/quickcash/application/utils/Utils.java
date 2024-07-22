32509a09c7fcc659a3f4975fabc3215e0440b405
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-22 13:26:42 -0300

    feat: utils base class

---
+++ src/main/java/br/com/gjsoftware/quickcash/application/utils/Utils.java
@@ -0,0 +1,9 @@
+package br.com.gjsoftware.quickcash.application.utils;
+
+import java.util.UUID;
+
+public class Utils {
+    public static String UUID() {
+        return UUID.randomUUID().toString();
+    }
+}
