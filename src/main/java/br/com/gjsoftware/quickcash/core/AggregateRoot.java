58e15b76a51a7fe57131988b7e4697f15816c2cc
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-22 13:24:29 -0300

    chore: aggregate root file

---
+++ src/main/java/br/com/gjsoftware/quickcash/core/AggregateRoot.java
@@ -0,0 +1,9 @@
+package br.com.gjsoftware.quickcash.core;
+
+import java.util.Date;
+
+public class AggregateRoot {
+    protected String _id;
+    protected Date createdAt;
+    protected Date updatedAt;
+}
