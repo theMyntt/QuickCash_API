15eb7acdc0ed0a119538ae4d9c502b5a884dc7c8
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-22 13:41:44 -0300

    fix: @Data annotation

--- src/main/java/br/com/gjsoftware/quickcash/core/AggregateRoot.java
+++ src/main/java/br/com/gjsoftware/quickcash/core/AggregateRoot.java
@@ -1,7 +1,10 @@
 package br.com.gjsoftware.quickcash.core;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class AggregateRoot {
     protected String _id;
     protected Date createdAt;
